b1646641b5f68e2a96e3a8eb3b589b11be1d23da
Michael Farnik <michaelfarnik@gmail.com>
2021-11-18 14:41:48 +0000

    WhileC

--- while/src/WhileC.hs
+++ while/src/WhileC.hs
@@ -8,16 +8,28 @@ import BamAST
 import qualified Parser(parse)
 
 aritToInstrs :: AExpr -> Code -> Code
-aritToInstrs = undefined
+aritToInstrs (EVar x) = [IFetch x]
+aritToInstrs (EInt n) = [IPush n]
+aritToInstrs (AAdd op a b) = [aritToInstrs a, aritToInstrs b, IAdd]
+aritToInstrs (ASub op a b) = [aritToInstrs a, aritToInstrs b, ISub]
+aritToInstrs (AMul op a b) = [aritToInstrs a, aritToInstrs b, IMul]
 
 boolToInstrs :: BExpr -> Code -> Code
-boolToInstrs = undefined
+boolToInstrs (BBool x) = [IFetch x]
+boolToInstrs (BNot x) = [boolToInstrs x, INot]
+boolToInstrs (BAnd a b) = [boolToInstrs a, boolToInstrs b, IAnd]
+boolToInstrs (AEq a b) = [boolToInstrs a, boolToInstrs b, IEq]
+boolToInstrs (ALe a b) = [boolToInstrs a, boolToInstrs b, ILe]
 
 boolToCode :: BExpr -> Code
 boolToCode b = boolToInstrs b []
 
 stmtToInstrs :: Stmt -> Code -> Code
-stmtToInstrs = undefined
+stmtToInstrs SSkip = []
+stmtToInstrs (SAssign v e) = [aritToInstrs e, IStore v]
+stmtToInstrs (SIte e a b) = [boolToInstrs e, IBranch a b]
+stmtToInstrs (SWhile c s) = [ILoop c s]
+stmtToInstrs (SSeq a b) = [a, b]
 
 compile :: Stmt -> Code
 compile s = stmtToInstrs s []
