2b8d770a502f98408485abe47348547da7c1c4c0
Michael Farnik <michaelfarnik@gmail.com>
2021-11-18 13:19:28 +0000

    WhilePG

--- while/src/WhilePG.hs
+++ while/src/WhilePG.hs
@@ -1,20 +1,44 @@
 module WhilePG where
 
 progA :: String
-progA = undefined
+progA = "n := n + 2"
 
 progB :: String
-progB = undefined
+progB = "if (n > 0) do\n
+    r := n\n
+  else\n
+    r := -n"
 
 progC :: String
-progC = undefined
+progC = "a := a - b\n
+  b := a + b\n
+  b := b - a\n
+  a := b - a"
 
 progD :: String
-progD = undefined
+progD = "r := 0\n
+  while (n != 0) {\n
+    if (n < -2) do\n
+      n := n - 2\n
+    else {\n
+      n := n - 1\n
+      r:= 1\n
+    }\n
+  }"
 
 progE :: String
-progE = undefined
+progE = "while (true) n := n + 1"
 
 progFact :: String
-progFact = undefined
-
+progFact = "if (n < 0) do {\n
+    n := -n\n
+    negative := true\n
+  } else\n
+    negative := false\n
+  r := 1\n
+  while (1 <= n) {\n
+    r := n * r\n
+    n := n - 1\n
+  }\n
+  if (negative = true) do\n
+    r := -r"
