5876a4aaa4cba1bcc6de93ecf677cb1adae735e9
Michael Farnik <michaelfarnik@gmail.com>
2021-12-02 14:59:44 +0000

    Added Geodel implementation

--- while/src/Goedel.hs
+++ while/src/Goedel.hs
@@ -5,25 +5,33 @@ import Parser (parse)
 import Data.Either (fromRight)
 
 encodeBool :: Bool -> Integer
-encodeBool = undefined
+encodeBool False = 0
+encodeBool True = 1
 
 decodeBool :: Integer -> Bool
-decodeBool = undefined
+decodeBool x = mod x 2 == 1
 
 delta :: Integer -> Integer
-delta = undefined
+delta x = 1/2 * x * (x + 1)
 
 pairHU :: Integer -> Integer -> Integer
-pairHU = undefined
+pairHU i j = delta(i+j-2)+i
 
 unpairHU :: Integer -> (Integer, Integer)
-unpairHU = undefined
+unpairHU h = (i, j)
+  where
+    c	=	floor(sqrt 2h - 1/2)
+    i = h-delta(c)
+    j = c-i+2
 
 pair :: Integer -> Integer -> Integer
-pair = undefined
+pair i j = pairHU (i + 1, j + 1) - 1
 
 unpair :: Integer -> (Integer, Integer)
-unpair = undefined
+unpair x = (i1 - 1, j1 - 1)
+  where 
+    k = x + 1
+    (i1, j1) = unpairHU k
 
 
 -- Encoding lists of numbers
@@ -55,10 +63,15 @@ tExample :: BTree
 tExample = foldr insert Empty [10, 8, 2, 5]
 
 encodeBTree :: BTree -> Integer
-encodeBTree = undefined
+encodeBTree Empty = 0
+encodeBTree (Fork n t1 t2) = 1 + pair(n, pair(encodeBTree t1, encodeBTree t2))
 
 decodeBTree :: Integer -> BTree
-decodeBTree = undefined
+decodeBTree 0 = Empty
+encodeBTree x = (n, t1, t2)
+  where 
+    (n, m) = unpair (x - 1)
+    (t1, t2) = unpair m
 
 
 -- Encoding variables
@@ -80,19 +93,56 @@ encodeAExpr (EBinOp ASub a1 a2) = 3 + 5 * pair (encodeAExpr a1) (encodeAExpr a2)
 encodeAExpr (EBinOp AMul a1 a2) = 4 + 5 * pair (encodeAExpr a1) (encodeAExpr a2)
 
 decodeAExpr :: Integer -> AExpr
-decodeAExpr = undefined
+decodeAExpr n = case r of
+  0 -> EVar (decodeVar (q / 5))
+  1 -> EInt q / 5
+  2 -> EBinOp AAdd i j
+  3 -> EBinOp ASub i j
+  4 -> EBinOp AMul i j
+  where
+    (q, r) = divMod n 5
+    i = decodeAExpr i1
+    j = decodeAExpr j1
+    (i1, j1) = unpair (q / 5)
 
 encodeBExpr :: BExpr -> Integer
-encodeBExpr = undefined
+encodeBExpr (BBool x) = 5 * encodeBool x
+encodeBExpr (BNot x) = 1 + 5 * encodeBExpr x
+encodeBExpr (BAnd a b) = 2 + 5 * pair (encodeBExpr a) (encodeBExpr b)
+encodeBExpr (BComp AEq a b) = 3 + 5 * pair (encodeBExpr a) (encodeBExpr b)
+encodeBExpr (BComp ALe a b) = 4 + 5 * pair (encodeBExpr a) (encodeBExpr b)
 
 decodeBExpr :: Integer -> BExpr
-decodeBExpr = undefined
+decodeBExpr n = case r of
+  0 -> BBool (encodeBool (q / 5))
+  1 -> BNot q / 5
+  2 -> BAnd i j
+  3 -> BComp AEq i j
+  4 -> BComp ALe i j
+  where
+    (q, r) = divMod n 5
+    i = decodeBExpr i1
+    j = decodeBExpr j1
+    (i1, j1) = unpair (q / 5)
 
 encodeStmt :: Stmt -> Integer
-encodeStmt = undefined
+encodeStmt SSkip = 0
+encodeStmt (SAssign x a) = 1 + 5 * pair (encodeVar x) (encodeAExpr a)
+encodeStmt (SIte b s1 s2) = 2 + 5 * pair (encodeBExpr b) (pair (encodeStmt s1) (encodeStmt s2))
+encodeStmt (SWhile b s) = 3 + 5 * pair (encodeBExpr b) (encodeStmt s)
+encodeStmt (SSeq s1 s2) = 4 + 5 * pair (encodeStmt s1) (encodeStmt s2)
 
 decodeStmt :: Integer -> Stmt
-decodeStmt = undefined
+decodeStmt n = case r of
+  0 -> SSkip
+  1 -> SAssign (decodeVar i) (decodeAExpr j)
+  2 -> SIte (decodeBExpr i) (decodeStmt j1) (decodeStmt j2)
+  3 -> SWhile (decodeBExpr i) (decodeStmt j)
+  4 -> SSeq (decodeStmt i) (decodeStmt j)
+  where
+    (q, r) = divMod n 5
+    (i, j) = unpair (q / 5)
+    (j1, j2) = unpair (j)
 
 astExample1 :: Stmt
 astExample1 =
